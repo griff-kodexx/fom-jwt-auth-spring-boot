9e7ad813399d10a6a4b7f4b6a9462fa62af8ef2c
griff <griffinsonsongo@gmail.com>
2018-07-20 16:37:55 +0300

    crud person object using jpa + rest print hello world

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+/*
+ *  Created by griff on 7/20/18 .
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String [] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
